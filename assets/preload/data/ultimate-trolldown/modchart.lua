759327aab9c3059d10e12cce5dac36a1b91b9edd
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-04 11:24:00 -0300

    Add files via upload

---
+++ assets/preload/data/ultimate-trolldown/modchart.lua
@@ -0,0 +1,12 @@
+function start (song)
+	changeDadCharacter("trolio")
+    changeDadCharacter("troil")
+end
+
+
+
+function stepHit (step)
+    if step == 1344 then
+        changeDadCharacter("trolio")
+    end
+end
