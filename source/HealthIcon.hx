a2bd082b383377aae2a9212c4f6511a0646a5735
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-03-03 21:04:50 -0300

    Add files via upload

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -37,6 +37,32 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+        animation.add('bf', [0, 1], 0, false, isPlayer);
+		animation.add('bftroll', [2, 3], 0, false, isPlayer);
+		animation.add('bfbig', [20, 21], 0, true, isPlayer);
+		animation.add('ron', [0, 1], 0, false, isPlayer);
+		animation.add('trolling', [2, 3], 0, false, isPlayer);
+		animation.add('gmod', [30, 31], 0, false, isPlayer);
+		animation.add('thai', [32, 32], 0, false, isPlayer);
+		animation.add('skit', [33, 33], 0, false, isPlayer);
+		animation.add('whitty', [24, 25], 0, false, isPlayer);
+		animation.add('trolling2', [4, 5], 0, false, isPlayer);
+		animation.add('trolling3', [6, 7], 0, false, isPlayer);
+		animation.add('trolling2e', [4, 5], 0, false, isPlayer);
+		animation.add('magneto', [8, 9], 0, false, isPlayer);
+		animation.add('phase8', [8, 9], 0, false, isPlayer);
+		animation.add('no-bobux', [10, 11], 0, false, isPlayer);
+		animation.add('notiky', [19, 20], 0, false, isPlayer);
+		animation.add('larg-boi', [19, 20], 0, false, isPlayer);
+		animation.add('other', [41, 41], 0, false, isPlayer);
+		animation.add('fimnall', [16, 17], 0, false, isPlayer);
+		animation.add('troil', [16, 17], 0, false, isPlayer);
+		animation.add('trolio', [26, 27], 0, false, isPlayer);
+		animation.add('ttto', [18, 18], 0, false, isPlayer);
+		animation.add('nottiky', [19, 20], 0, false, isPlayer);
+		animation.add('happy', [0, 1], 0, true, isPlayer);
+		animation.add('smile', [28, 29], 0, false, isPlayer);
+		animation.add('alone', [22, 23], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
